b47d161f2c35fd26321615438e5a417f0f1b20e4
RaquelLima7 <raquellimadev@gmail.com>
2022-01-26 19:51:02 -0300

    create_list

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Order, type: :model do
   end
 
   it 'Possui 3 pedidos - create_list' do
-    orders = create_list(:order, 3)
+    orders = create_list(:order, 3) # para não ficar repitindo o n° de orders, vai colocar create_list e dizer o quanto que quer
     expect(orders.count).to eq(3)
   end
 
