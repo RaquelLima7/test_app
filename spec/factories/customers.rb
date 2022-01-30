24930788cef066fc5b754127f3a841a89ccb6a89
RaquelLima7 <raquellimadev@gmail.com>
2022-01-30 18:23:41 -0300

    create model categories and products

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :customer, aliases: [:user] do
 
-    transient do
-      upcased false
-      qtt_orders 3
-    end
+    # transient do
+    #   upcased false
+    #   qtt_orders 3
+    # end
 
     #toda vez que passar um atributo dinâmico passar em bloco
     name { Faker::Name.name }
@@ -13,46 +13,46 @@ FactoryBot.define do
     #email { Faker::Internet.email }
     sequence(:email) { |n| "meu_email-#{n}@email.com" }
 
-    trait :male do
-      gender 'M'
-    end
+    # trait :male do
+    #   gender 'M'
+    # end
 
-    trait :female do
-      gender 'F'
-    end
+    # trait :female do
+    #   gender 'F'
+    # end
 
-    trait :vip do
-      vip true
-      days_to_pay 30
-    end
+    # trait :vip do
+    #   vip true
+    #   days_to_pay 30
+    # end
 
-    trait :default do
-      vip false
-      days_to_pay 15
-    end
+    # trait :default do
+    #   vip false
+    #   days_to_pay 15
+    # end
 
-    # Para poder criar vários pedidos no momento em que invocar um cliente, começa criando uma trait
-    # o padrão é 3 (linha 6) 
-    trait :with_orders do
-      after(:create) do |customer, evaluator|
-       create_list(:order, evaluator.qtt_orders, customer: customer) # evaluator e o qtt_orders que é o transient informado na linha 4
-      end
-      # Cria uma lista de pedidos com a quantidade que a pessoa enviar, se não enviar vao ser o 3
-      # que é o valor informado no transient da linha 4 e o cliente vai ser o próprio da fábrica.
-    end
+    # # Para poder criar vários pedidos no momento em que invocar um cliente, começa criando uma trait
+    # # o padrão é 3 (linha 6) 
+    # trait :with_orders do
+    #   after(:create) do |customer, evaluator|
+    #    create_list(:order, evaluator.qtt_orders, customer: customer) # evaluator e o qtt_orders que é o transient informado na linha 4
+    #   end
+    #   # Cria uma lista de pedidos com a quantidade que a pessoa enviar, se não enviar vao ser o 3
+    #   # que é o valor informado no transient da linha 4 e o cliente vai ser o próprio da fábrica.
+    # end
 
-    factory :customer_with_orders, traits: [:with_orders]
-    factory :customer_male, traits: [:male]
-    factory :customer_female, traits: [:female]
-    factory :customer_vip, traits: [:vip]
-    factory :customer_default, traits: [:default]
-    factory :customer_male_vip, traits: [:male, :vip]
-    factory :customer_female_vip, traits: [:female, :vip]
-    factory :customer_male_default, traits: [:male, :default]
-    factory :customer_female_default, traits: [:female, :default]
+    # factory :customer_with_orders, traits: [:with_orders]
+    # factory :customer_male, traits: [:male]
+    # factory :customer_female, traits: [:female]
+    # factory :customer_vip, traits: [:vip]
+    # factory :customer_default, traits: [:default]
+    # factory :customer_male_vip, traits: [:male, :vip]
+    # factory :customer_female_vip, traits: [:female, :vip]
+    # factory :customer_male_default, traits: [:male, :default]
+    # factory :customer_female_default, traits: [:female, :default]
 
-    after(:create) do |customer, evaluator|
-      customer.name.upcase! if evaluator.upcased
-    end
+    # after(:create) do |customer, evaluator|
+    #   customer.name.upcase! if evaluator.upcased
+    # end
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,60 +3,60 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   
   #vc quer um customer, mas quer que o nome dele seja e passa o nome
-  it '#full_name - Sobrescrevendo Atributo' do
-    customer = create(:customer, name: "Jackson Pires" ) #Factorybot 
-    customer1 = create(:customer, name: "Jackson Pires" )
-    expect(customer.full_name).to eq("Sr. Jackson Pires")
-  end
+  # it '#full_name - Sobrescrevendo Atributo' do
+  #   customer = create(:customer, name: "Jackson Pires" ) #Factorybot 
+  #   customer1 = create(:customer, name: "Jackson Pires" )
+  #   expect(customer.full_name).to eq("Sr. Jackson Pires")
+  # end
 
-  it 'Herança' do
-    customer = create(:customer_vip)
-    expect(customer.vip).to eq(true)
-  end
+  # it 'Herança' do
+  #   customer = create(:customer_vip)
+  #   expect(customer.vip).to eq(true)
+  # end
 
-  it '#full_name' do
-    #nesse caso está user, pq colocou no customers.rb em factories o aliases
-    customer = create(:user) # ou create(:customer)
-    expect(customer.full_name).to start_with("Sr. ")
-  end
+  # it '#full_name' do
+  #   #nesse caso está user, pq colocou no customers.rb em factories o aliases
+  #   customer = create(:user) # ou create(:customer)
+  #   expect(customer.full_name).to start_with("Sr. ")
+  # end
 
-  #Utilizando essa palavra ele vai trazer apenas os atributos em um hash do elemento que pediu
-  it 'Usando o attributes_for' do
-    attrs = attributes_for(:customer) # vai factories vai procurar o customer e vai trazer os atribuitos desse elemento
-    customer = Customer.create(attrs) # vai criar o customer com esses atributos
-    expect(customer.full_name).to start_with("Sr. ")
-  end
+  # #Utilizando essa palavra ele vai trazer apenas os atributos em um hash do elemento que pediu
+  # it 'Usando o attributes_for' do
+  #   attrs = attributes_for(:customer) # vai factories vai procurar o customer e vai trazer os atribuitos desse elemento
+  #   customer = Customer.create(attrs) # vai criar o customer com esses atributos
+  #   expect(customer.full_name).to start_with("Sr. ")
+  # end
 
-  it 'Atributo Transitório' do
-    customer = create(:customer_default, upcased: true)
-    expect(customer.name.upcase).to eq(customer.name)
-  end
+  # it 'Atributo Transitório' do
+  #   customer = create(:customer_default, upcased: true)
+  #   expect(customer.name.upcase).to eq(customer.name)
+  # end
 
-  it 'Cliente Feminino' do
-    customer = create(:customer_female)
-    expect(customer.gender).to eq('F')
-  end
+  # it 'Cliente Feminino' do
+  #   customer = create(:customer_female)
+  #   expect(customer.gender).to eq('F')
+  # end
 
-  it 'Cliente Feminino Default' do
-    customer = create(:customer_female_default)
-    expect(customer.gender).to eq('F')
-  end
+  # it 'Cliente Feminino Default' do
+  #   customer = create(:customer_female_default)
+  #   expect(customer.gender).to eq('F')
+  # end
 
-  it 'Cliente Masculino Vip' do
-    customer = create(:customer_male_vip)
-    expect(customer.gender).to eq('M')
-    expect(customer.vip).to eq(true)
-  end
+  # it 'Cliente Masculino Vip' do
+  #   customer = create(:customer_male_vip)
+  #   expect(customer.gender).to eq('M')
+  #   expect(customer.vip).to eq(true)
+  # end
 
-  it 'travel_to' do
-    travel_to Time.zone.local(2004, 05, 28, 01, 04, 44) do
-      @customer = create(:customer_vip)
-    end
+  # it 'travel_to' do
+  #   travel_to Time.zone.local(2004, 05, 28, 01, 04, 44) do
+  #     @customer = create(:customer_vip)
+  #   end
 
-    expect(@customer.created_at).to be < Time.now
-  end
+  #   expect(@customer.created_at).to be < Time.now
+  # end
 
-  #na hora que cria um customer vai estar um no model e criando um novo cliente e vai mudar 
-  #a quantidade de clientes em 1
-  it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
+  # #na hora que cria um customer vai estar um no model e criando um novo cliente e vai mudar 
+  # #a quantidade de clientes em 1
+  # it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
 end
